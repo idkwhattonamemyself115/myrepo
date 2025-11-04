dfeef0bda0034a4469d8a211b97d106f92222b1d
idkwhattonamemyself115 <khu27@nmhschool.org>
2025-11-03 23:14:03 -0500

    hw d1

---
+++ iteration_05/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import jinja2
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/contact")
+def contact():
+    phone_number = None
+    is_student=True
+    email = "khu27@nmhschool.org"
+    return render_template("contact.html", email=email, is_student=is_student, phone_number=phone_number)
+@app.route("/tools_used")
+def tool_used():
+    tools = ["Flask", "Jinja2", "Python"]
+    return render_template("tools_used.html", tools=tools)
+
+@app.route("/about")
+def about():
+    work_in_tech=True
+    techonological_companies= ["Google", "Apple", "Microsoft", "Amazon", "Facebook"]
+    return render_template("about.html", author=" ",techonological_companies=techonological_companies, work_in_tech=work_in_tech, is_student=True)
+
+app.run(debug=True)
